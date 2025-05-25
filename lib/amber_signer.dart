810e93b982ee1a3ed4076de272a2f2d426c69556
franzap <_@franzap.com>
2025-05-25 01:06:35 -0300

    Update to new Signer interface

--- lib/amber_signer.dart
+++ lib/amber_signer.dart
@@ -4,70 +4,73 @@ import 'package:models/models.dart';
 import 'package:signer_plugin/signer_plugin.dart';
 
 class AmberSigner extends Signer {
-  final _signerPlugin = SignerPlugin();
-  bool isAvailable = false;
+  SignerPlugin? _signerPlugin;
 
   AmberSigner(super.ref);
 
   @override
-  Future<AmberSigner> initialize() async {
-    final hasExternalSigner = await _signerPlugin.isExternalSignerInstalled(
+  Future<void> initialize({bool active = true}) async {
+    _signerPlugin = SignerPlugin();
+    final hasExternalSigner = await _signerPlugin!.isExternalSignerInstalled(
       'com.greenart7c3.nostrsigner',
     );
     if (hasExternalSigner) {
-      await _signerPlugin.setPackageName('com.greenart7c3.nostrsigner');
-      isAvailable = true;
+      await _signerPlugin!.setPackageName('com.greenart7c3.nostrsigner');
+    } else {
+      // trigger failure in super
+      return super.initialize();
     }
-    return this;
+
+    final map = await _signerPlugin!.getPublicKey();
+    final npub = map['npub'] ?? map['result'];
+    if (npub != null) {
+      internalSetPubkey(Utils.hexFromNpub(npub));
+    }
+
+    return super.initialize(active: active);
   }
 
   @override
-  Future<String?> getPublicKey() async {
-    try {
-      final map = await _signerPlugin.getPublicKey();
-      final npub = map['npub'] ?? map['result'];
-      if (npub != null) {
-        addSignedInPubkey(Utils.hexFromNpub(npub));
-      }
-      return npub;
-    } catch (e) {
-      return null;
-    }
-  }
-
-  @override
-  Future<E> sign<E extends Model<E>>(
-    PartialModel<E> partialModel, {
-    String? withPubkey,
-  }) async {
-    if (!isAvailable) {
+  Future<List<E>> sign<E extends Model<dynamic>>(
+    List<PartialModel<Model<dynamic>>> partialModels,
+  ) async {
+    if (!isInitialized) {
       throw Exception("Cannot sign, missing Amber");
     }
 
-    final pubkey = withPubkey ?? await getPublicKey();
+    final signedModels = <E>[];
 
-    if (partialModel is PartialDirectMessage) {
-      final signedMessage = await _signerPlugin.nip04Encrypt(
-        partialModel.event.content,
+    for (final partialModel in partialModels) {
+      if (partialModel is PartialDirectMessage) {
+        final signedMessage = await _signerPlugin!.nip04Encrypt(
+          partialModel.event.content,
+          "",
+          pubkey!,
+          partialModel.event.getFirstTagValue('p')!,
+        );
+        final encryptedContent = signedMessage['result'];
+        partialModel.event.content = encryptedContent;
+      }
+
+      // Remove all null fields (Amber otherwise crashes)
+      final map = {
+        for (final e in partialModel.toMap().entries)
+          if (e.value != null) e.key: e.value,
+      };
+      final signedMessage = await _signerPlugin!.signEvent(
+        jsonEncode(map),
         "",
-        withPubkey!,
-        (partialModel as PartialDirectMessage).event.getFirstTagValue('p')!,
+        pubkey!,
       );
-      final encryptedContent = signedMessage['result'];
-      partialModel.event.content = encryptedContent;
+      final signedMap = jsonDecode(signedMessage['event']);
+      signedModels.add(Model.getConstructorFor<E>()!.call(signedMap, ref));
     }
+    return signedModels;
+  }
 
-    // Remove all null fields (Amber otherwise crashes)
-    final map = {
-      for (final e in partialModel.toMap().entries)
-        if (e.value != null) e.key: e.value,
-    };
-    final signedMessage = await _signerPlugin.signEvent(
-      jsonEncode(map),
-      "",
-      pubkey!,
-    );
-    final signedMap = jsonDecode(signedMessage['event']);
-    return Model.getConstructorFor<E>()!.call(signedMap, ref);
+  @override
+  Future<void> dispose() {
+    _signerPlugin = null;
+    return super.dispose();
   }
 }
